a0a0fc7c80a670c931a00a88778295aae1356898
EdersonDavi <ederson.silva@sunoresearch.com.br>
2022-07-19 07:26:48 -0300

    feat: init server and exec first test

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyparser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
+import { Application } from 'express';
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyparser.json())
+  }
+
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([
+      forecastController
+    ])
+  }
+
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+}
