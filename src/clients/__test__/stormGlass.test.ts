3b26553474e566cff513be2199c77000be001253
EdersonDavi <ederson.silva@sunoresearch.com.br>
2022-07-20 14:38:08 -0300

    feat: mocked axios

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,13 +6,15 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.32555;
     const lng = 152.32555;
 
-    axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture })
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture })
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
 
     const response = await stormGlass.fetchPoints(lat, lng);
 
