e40bb3479ca1f9a6adde3a7d2caab72a73d09cef
EdersonDavi <ederson.silva@sunoresearch.com.br>
2022-08-19 06:51:13 -0300

    user authenticat

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,7 @@ import { Post, Controller } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { BaseController } from '.';
 import { User } from '../models/User';
+import AuthService from '@src/services/auth';
 
 
 @Controller('users')
@@ -18,5 +19,30 @@ export class UserController extends BaseController {
       this.sendCreatedUpdatedErrorResponse(res, error as Error)
     }
 
+  }
+
+  @Post('authenticate')
+  public async authenticate(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(401).send({
+        code: 401,
+        error: 'User not found'
+      })
+    }
+
+    const passwordAuthenticate = await AuthService.comparePassword(password, user.password)
+
+    if (!passwordAuthenticate) {
+      return res.status(401).send({
+        code: 401,
+        error: 'Password dos not match!'
+      })
+    }
+
+    const token = AuthService.generateToken(user.toJSON())
+    return res.status(200).send({ token })
   }
 }
